8ed21f340108a84b6f3bc5d0c522c3a41acbdcee
Jerome Johnson <JohnsonJeromeC@gmail.com>
2014-10-24 14:16:57 -0500

    add quantity column to cart for multiple purchases of same item, changed cart layout to table, added totals, added method to empty cart, handled invalid cart errors

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,20 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
+
+
+  def add_product(product_id)
+    curent_item = line_items.find_by(product_id: product_id)
+
+    if curent_item
+      curent_item.quantity +=1
+    else
+      curent_item = line_items.build(product_id: product_id)
+    end
+    
+    curent_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 end
