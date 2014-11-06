e8e8ee27a9f9727575bd6171dc0e0cdce72e4779
Jerome Johnson <JohnsonJeromeC@gmail.com>
2014-11-06 09:15:05 -0600

    add first iteration of checkout, capture order details and associate them with order model, add validation to order model, created ATOM xml feed to keep track of orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
-
   resources :carts
 
   get 'store/index'
   match 'store/index', to: 'carts#destroy', via: :post
 
-  resources :products
+  resources :products do 
+    get :who_bought, on: :member
+  end
 
   root to:'store#index', as:'store'
 
